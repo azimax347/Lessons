934420def13f19270b367b4f683f2fe44d8490a9
azimax347 <the.azimax@gmail.com>
2022-04-20 21:17:28 +0700

    Различные выводы информации

---
+++ Урок_2.java
@@ -0,0 +1,13 @@
+public class Урок_2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world"); // Вывод текста с послед. переводом на новую строку (ln)
+        System.out.print("Hello world \n"); // Перевод на следующую строку через "\n"
+        System.out.print("test");
+
+        int x = 10;
+        double y = 35.3256723;
+        System.out.printf("x = %d; y = %.3f", x, y); // Вывод через "printf"
+
+    }
+}
