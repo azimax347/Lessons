ee87f741064fbea07d56187044bd04cb1fc456a4
azimax347 <the.azimax@gmail.com>
2022-04-22 18:14:53 +0700

    ・

--- Урок_3.java
+++ Урок_3.java
@@ -4,16 +4,15 @@ public class Урок_3 {
     public static void main(String[] args) {
 
         Scanner novi = new Scanner(System.in); // Объявление сканера с названием "novi"
+        System.out.println("Число больше 10 ?");
+        String z = novi.nextLine();
+        System.out.println("Твой ответ" + z);
         System.out.println("Введите первое число: ");
         int x = novi.nextInt(); // Считывание целого числа с консоли
         System.out.println("Введите второе число: ");
         int y = novi.nextInt();
         System.out.printf("Твоё число: %d \n", x + y);
-
-        Scanner novi2 = new Scanner(System.in);
-        System.out.println("Число больше 10 ?");
-        String z = novi2.nextLine();
-        System.out.println(z);
+        novi.close();
     }
 
 }
