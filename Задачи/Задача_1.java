b02ba11e2f8c1ce7384159d55c126bd84050bbaf
azimax347 <the.azimax@gmail.com>
2022-04-24 05:43:42 +0700

    Задача на циклы

--- Задачи/Задача_1.java
+++ Задачи/Задача_1.java
@@ -12,6 +12,7 @@ public class Задача_1 {
         int y = scan.nextInt();
         System.out.println("Введите число смертности в год на тысячу человек7");
         int z = scan.nextInt();
+        scan.close();
         int q = y - z;
         for (int i = 1; i <= 10; i++) {
             x += x * q / 1000;
