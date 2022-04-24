6e26724fbb0366b1c4a62fbf05053b041ccdaddf
azimax347 <the.azimax@gmail.com>
2022-04-24 18:50:03 +0700

    Задача на циклы

--- Задачи/Задача_2.java
+++ Задачи/Задача_2.java
@@ -1,16 +1,23 @@
 package Задачи;
 
+import java.util.Scanner;
+
 public class Задача_2 {
     public static void main(String[] args) {
         int d = 1;
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
+        Scanner skan = new Scanner(System.in);
+        System.out.print("Число строк: ");
+        int x = skan.nextInt();
+        System.out.print("Число столбов: ");
+        int y = skan.nextInt();
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
                 d = i * j;
                 System.out.printf("%d\t", d);
             }
             System.out.println();
-
         }
+        skan.close();
     }
 
 }
