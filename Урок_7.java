f74c23c263ad60ce9a5dd13b74ee0570635e75f6
azimax347 <the.azimax@gmail.com>
2022-04-25 20:41:05 +0700

    Метод без возвращения

---
+++ Урок_7.java
@@ -0,0 +1,14 @@
+public class Урок_7 {
+    public static void main(String[] args) {
+        test1(10, 20); // * Вызов метода test1
+
+    }
+
+    static void test1(int x, int y) { // * Метод с двумя параметрами int
+        System.out.println("Вывод метода тест 1");
+        int z = x + y;
+        System.out.println(z);
+
+    }
+
+}
