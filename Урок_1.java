b4079fcf0ef50d2fe619afd966a641e9d013446a
azimax347 <the.azimax@gmail.com>
2022-04-30 21:56:21 +0700

    Типы данных

--- Урок_1.java
+++ Урок_1.java
@@ -5,11 +5,11 @@ public class Урок_1 { // Имя файла
         int y = 45; // Объявление + присвоение
         System.out.println(x); // Вывод 10
         System.out.println(y); // Вывод 45
-        var z = 23.34; // Сам определяет тип переменной
-        System.out.println(z);
         final var q = 93; // Объявление константы
         System.out.println(q);
         String h = "hello world";
         System.out.println(h);
+        var z = 23.34; // Сам определяет тип переменной
+        System.out.println(z);
     }
 }
